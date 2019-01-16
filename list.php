31dcbccc8373d61b85c6aae7f8bd5520bf4a6a1b
sachins0412 <saxenas964@gmail.com>
2019-01-16 16:53:16 +0530

    mindTwisted

---
+++ list.php
@@ -0,0 +1,73 @@
+<?php include 'navbar.php';
+$sql="SELECT * FROM category";
+$result=mysqli_query($conn,$sql);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<script>
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+$(document).ready(function(){
+  $("#myInput").focus(function(){
+    $(this).css("background-color", "#cccccc");
+  });
+  $("#myInput").blur(function(){
+    $(this).css("background-color", "#ffffff");
+  });
+});</script>
+	<title></title>
+</head>
+<body>
+	<div class="container mt-5 ">
+	<form class="form-inline my-2 my-lg-0">
+      <input class="form-control-sm " id="myInput" type="search" placeholder="Search in this table" aria-label="Search">
+      
+    </form><br>
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">catid</th>
+      <th scope="col"> Name</th>
+      <th scope="col" style="padding-left: 40px;">Subcat</th>
+    </tr>
+  </thead>
+  <tbody id="myTable">
+  	<?php
+    if(mysqli_num_rows($result)>0){
+    	while($row=mysqli_fetch_row($result)){
+    		$sql2= "SELECT name FROM subcategory WHERE catid='$row[0]'";
+    		$result2=mysqli_query($conn,$sql2);
+    	?>
+    <tr>	
+    	
+      <th scope="row"><?php echo $row[0];?></th>
+      <td><?php echo $row[1];?></td>
+      <td>
+      	<ul><?php 
+      	if(mysqli_num_rows($result2)>0){
+      		while($row1=mysqli_fetch_row($result2)){?>
+      		<li><?php echo $row1[0];?></li>
+      		<?php		
+      		}
+      	}
+      	 ?>
+      </ul></td>
+
+    </tr>
+<?php
+}
+    } 
+    ?>
+  </tbody>
+</table>
+</div>
+</body>
+</html>
---
+++ newsubcatcreated.php
@@ -0,0 +1,19 @@
+<?php
+include "navbar.php";
+$catid=$_POST['catid'];
+$subname=$_POST['subcatname'];
+$sql="INSERT INTO subcategory(name,catid) VALUES('$subname','$catid')";
+$result=mysqli_query($conn,$sql);
+if($result){
+	echo "inserted succesfully";?>
+	<html>
+	<head></head>
+	<title></title>
+	<body><a href=index.php><button>go back</button></a>
+	</body>
+	</html>
+<?php }
+else{
+	echo "failed with stupid error";
+}
+?>
