2945b3fcec6ed2517a2ba30f66a665ed5f97a7ea
sachins0412 <saxenas964@gmail.com>
2019-01-17 13:49:29 +0530

    booyeah

--- stockin.php
+++ stockin.php
@@ -40,14 +40,14 @@ $count=mysqli_num_rows($result2);
     <p class="card-text"><form method="POST">
   <div class="form-group">
     <label>StockIn</label>
-    <input type="text" class="form-control" name="quan" id="quan" placeholder="Enter Quantity">
+    <input type="text" class="form-control" name="quan" id="quan" placeholder="Enter Quantity" onchange="myfun2(this.value)">
     
   </div>
   <div class="row">
   	<div class="col-sm-6">
   <div class="form-group">
     <label>UnitCost</label>
-    <input type="text" class="form-control" name="unit" placeholder="Cost per unit" onchange="myfun(this.value)">
+    <input type="text" class="form-control" name="unit" id="cost" placeholder="Cost per unit" onchange="myfun(this.value)">
   </div>
 </div>
 <div class="col-sm-6">
@@ -65,31 +65,50 @@ $count=mysqli_num_rows($result2);
 </div>
 		</div>
 		<div class="col-md-6">
-		<div class="card">
+			<div class="card">
   <div class="card-header">
     <strong>StockOut</strong>
   </div>
   <div class="card-body">
+  	<div class="row">
+  		<div class="col-sm-6">
     <h5 class="card-title">Category: <span style="font-weight: 10;font-family: TIMES"><?php echo ucfirst($row[0]); ?></span></h5>
-    
-    <h5 class="card-title">SubCategory: <span style="font-weight: 10;font-family: TIMES"><?php echo ucfirst($subcat); ?>
-    	</span></h5>
-    
-    <p class="card-text"><form>
+    <h5 class="card-title">SubCategory: <span style="font-weight: 10;font-family: TIMES"><?php echo ucfirst($subcat); ?></span></h5>
+    </div>
+    <div class="col-sm-6">
+    	<h5 class="card-title">Available: <span style="font-weight: 10;font-family: TIMES"><?php
+    	if($count>0) echo $row[3];
+    	else 
+    	echo 0; ?></span></h5>
+    </div>
+</div>
+    <p class="card-text"><form method="POST">
   <div class="form-group">
-    <label>StockIn</label>
-    <input type="text" class="form-control" placeholder="Enter Quantity">
+    <label>StockOut</label>
+    <input type="text" class="form-control" name="quan2" id="quan2" placeholder="Enter Quantity" onchange="myfun3(this.value)">
     
   </div>
+  <div class="row">
+  	<div class="col-sm-6">
   <div class="form-group">
     <label>UnitCost</label>
-    <input type="text" class="form-control" placeholder="Cost per unit">
+    <input type="text" class="form-control" name="unit2" id="cost2" placeholder="Cost per unit" onchange="myfun4(this.value)">
   </div>
-  
+</div>
+<div class="col-sm-6">
+  <div class="form-group">
+    <label>TotalCost</label>
+    <input type="text" class="form-control" id="total2" disabled value="" >
+    
+  </div>
+</div>
+</div>  
   <button type="submit" class="btn btn-primary">Submit</button>
 </form></p>
     
   </div>
+</div>
+		</div>
 </div>
 <script type="text/javascript">
 	function myfun(cost){
@@ -97,10 +116,36 @@ $count=mysqli_num_rows($result2);
 		if(quan){
 			var total= cost*quan;
 		}
-		else{
-			var total=0;
-		}
+		else
+			total=0;
 		document.getElementById('total').setAttribute("value",total);
+	}
+	function myfun2(quan){
+		var cost=document.getElementById('cost').value;
+		if(cost){
+			var total= cost*quan;
+		}
+		else
+			total=0;
+		document.getElementById('total').setAttribute("value",total);
+	}
+	function myfun4(cost2){
+		var quan2=document.getElementById('quan2').value;
+		if(quan2){
+			var total2= cost2*quan2;
+		}
+		else
+			total2=0;
+		document.getElementById('total2').setAttribute("value",total2);
+	}
+	function myfun3(quan2){
+		var cost2=document.getElementById('cost2').value;
+		if(cost2){
+			var total2= cost2*quan2;
+		}
+		else
+			total2=0;
+		document.getElementById('total2').setAttribute("value",total2);
 	}
 </script>
 </body>
