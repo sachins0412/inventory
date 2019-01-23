11b1649fe1c98135e2b588b08d5ceb0095a231f9
sachins0412 <saxenas964@gmail.com>
2019-01-23 15:51:54 +0530

    kyahora

--- transaction.php
+++ transaction.php
@@ -146,12 +146,12 @@ $(document).ready(function(){
                 var from_date = $('#from_date').val();  
                 var to_date = $('#to_date').val();  
                 if(from_date != '' && to_date != '')  
-                {   
+                {   var subcat=document.getElementById("subcat").innerText;
     var catid=document.getElementById("catid").innerText;
                      $.ajax({  
                           url:"filter.php",  
                           method:"POST",  
-                          data:{from_date:from_date, to_date:to_date,catid:catid},  
+                          data:{from_date:from_date, to_date:to_date,catid:catid, subcat:subcat},  
                           success:function(data)  
                           {  
                                $('#myTable').html(data);  
